5dc05e92fb028814ed2089b87d2dfa51229b3637
Heath Blackmon <coleoguy@gmail.com>
2015-09-17 10:19:31 -0500

    misc updates

---
+++ future.work.R
@@ -0,0 +1,17 @@
+---
+  title: "Things to explore later"
+author: "Heath Blackmon"
+output: 
+  html_document:
+  theme: cosmo
+highlight: kate
+toc: true
+toc_depth: 2
+---
+
+
+[Return to main website](http://coleoguy.github.io/resources.html)
+
+simulate trees
+count number changes under state 1 versus on state 2
+
