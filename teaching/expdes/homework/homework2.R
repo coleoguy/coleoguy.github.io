68d9462bac8cbc6fb9ece007eeca4e9111312730
Heath Blackmon <coleoguy@gmail.com>
2022-09-20 07:51:38 -0500

    for tuesday

---
+++ teaching/expdes/homework/homework2.R
@@ -0,0 +1,7 @@
+dat <- read.csv("bettafish.csv")
+# with this data make a presentation ready plot
+# that demonstrates the impact of at least two 
+# of the predictor variables (sex, size, and color)
+# on the repsonse variable (price).
+
+
