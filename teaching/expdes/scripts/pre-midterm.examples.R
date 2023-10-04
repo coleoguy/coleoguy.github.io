7830430565db1bbb067562652cba24c8dfda60cc
Heath Blackmon <coleoguy@gmail.com>
2023-10-04 17:15:39 -0500

    add script

--- teaching/expdes/scripts/pre-midterm.examples.R
+++ teaching/expdes/scripts/pre-midterm.examples.R
@@ -1,5 +1,5 @@
 
-# Permutation Test
+# Permutation Test Example One
 set.seed(1)
 cave <- rpois(n = 20, lambda = 24)
 surf <- rpois(n = 20, lambda = 22)
@@ -13,7 +13,7 @@ plot(density(abs(null.dist)))
 abline(v=obs.dif)
 sum(abs(null.dist) >= obs.dif) / length(null.dist)
 
-
+# Permutation Test Example Two
 set.seed(5)
 crick1 <- rpois(15, lambda = 12)
 crick3 <- rpois(15, lambda = 10)
@@ -35,7 +35,7 @@ sum(abs(null.dist) >= abs(obs.dif)) / length(null.dist)
 # Mann-Whitney
 
 
-# You're lab has an old nanodrop that you aren't sure is working
+# Your lab has an old nanodrop that you aren't sure is working
 # However, you have 100s of extractions that need to be quantified
 # You decide to test your nanodrop against one in another lab that
 # is brand new. You picked 10 samples that you will quantify on both
@@ -45,6 +45,9 @@ old <- c(1.87, 1.91, 1.86, 1.81, 1.87,
 new <- c(1.74, 1.62, 1.73, 1.75, 1.88, 
          1.72, 1.86, 1.80, 1.77, 1.84)
 
+t.test(old, new, paired=T)
+
+
 # From years of observation you know that typically only 
 # 63% of males and 74% of female hedgehogs survive to one
 # year of age. You tagged 50 females and 50 males last year and
@@ -52,9 +55,29 @@ new <- c(1.74, 1.62, 1.73, 1.75, 1.88,
 # 24 males alive. Assume that all hedgehogs not documented 
 # as still alive are dead. Do the survival rates in your study
 # agree with long term trends?
+null <- rbinom(1000, 50, .63)
+plot(density(null))
+abline(v=24)
+sum(null <= 24)/1000
 
+binom.test(24, 50, .63)
+binom.test(38, 50, .74)
+# You are studying water purification failure. A town's 
+# water supply was contaminated leading to kidney problems
+# in some residents. You are interested in determining 
+# wether sexes are impacted differently. You found that
+# 1200 residents drank the contaminated water 632 were women
+# 568 were men. 57 men got ill while 95 women got sick.
+# Does sex have an impact on whether people get sick?
+95/632
+57/568
+x <- matrix(c(537, 511, 95, 57), 2, 2)
+colnames(x) <- c("healthy", "ill")
+row.names(x) <- c("female", "male")
+chisq.test(x)
 
-# You studying acyl sugar productions in tomato. You have 
-# measured 
-
+# Testing the effect of window angle on bird impact deaths. 
+# Treatments were vertical, 20, or 40 degrees off vertical. 
+# During the experiment 30, 15, and 8 birds
+# were killed by each type of window respectively
 
